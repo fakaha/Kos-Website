046ae842405c06c0c70bc25902426c94615453f8
fakaha <lunastra34@gmail.com>
2024-12-08 20:41:43 +0700

    feat: add bootstrap and font google

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
@@ -79,6 +85,239 @@
                 @endcomponent
                 --}}
             <!-- </div> -->
+             <div class="row">
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="{{ route('landing.details', 'coral-pik-seven') }}">
+                <img src="{{ asset('images/house1.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="{{ route('landing.details', 'waterloo') }}">
+                    <h3 class="small-header mb-2">
+                        Grand coast Office PIK
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    @php
+                        $hasBooked = true;
+                    @endphp
+                    <div class="price">
+                        <p class="mb-0">
+                            {{ $hasBooked ? 'sold out' : '$28,109' }}
+                            <span class="extra-small-paragraph">
+                                {{ $hasBooked ? '' : '/mo' }}
+                            </span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house2.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house3.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house4.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house5.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house6.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house7.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="item">
+            <a href="">
+                <img src="{{ asset('images/house8.png') }}" alt="" class="img-fluid">
+            </a>
+            <div class="info">
+                <a href="">
+                    <h3 class="small-header mb-2">
+                        Coral Brown
+                    </h3>
+                </a>
+                <div class="footer">
+                    <div class="location d-flex flex-row ">
+                        <img src="{{ asset('images/ic_loc.svg') }}" height="20" alt="">
+                        <p class="small-paragraph mb-0">
+                            Exnoparty
+                        </p>
+                    </div>
+                    <div class="price">
+                        <p class="mb-0">
+                            $28,109<span class="extra-small-paragraph">/mo</span>
+                        </p>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
         </div>
     </section>
 
